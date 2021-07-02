db7de61ff85d9517528429d133db1ba3dfd308dc
firstpitchstrike <johnturek2013@gmail.com>
2021-07-01 21:23:04 -0500

    Add files via upload

---
+++ Mark Prior.R
@@ -0,0 +1,18 @@
+### The Mark Prior Project
+library(tidyverse)
+library(Lahman)
+pitching
+mark_prior <- pitching %>%
+  filter(playerID == "priorma01")
+mark_prior
+
+library(ggplot2)
+ggplot(mark_prior, aes(x = yearID, y = W))+
+  geom_point()
+ggplot(mark_prior, aes(x = yearID, y= W)) +
+  labs(x = "Year", y = "Wins") +
+  geom_line()
+ggplot(mark_prior, aes(x = yearID, y = W)) +
+  labs(x = "Year", y = "Wins") +
+  geom_bar(stat = "identity")  ### had to add stat = identity
+rlang::last_trace()
